326cf6b48abc2705e121cc538dc2f78bc1fff3f2
anahitaaasadi <101253788+anahitaaasadi@users.noreply.github.com>
2022-09-21 09:45:47 +0430

    FIFO

---
+++ FIFO/FIFO256.vhd
@@ -0,0 +1,81 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:48:32 05/07/2021 
+-- Design Name: 
+-- Module Name:    ram256byte - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FIFO256 is port	(din: in std_logic_vector(7 downto 0);
+								clk_rd, clk_wr, wr, rd: in std_logic;
+								dout: out std_logic_vector(7 downto 0);
+								full, empty: out std_logic);
+end FIFO256;
+
+architecture Behavioral of FIFO256 is
+									type memory is array(0 to 255) of std_logic_vector(7 downto 0);
+									signal ram: memory;
+									signal rd_addr: std_logic_vector(7 downto 0):= "00000000";
+									signal wr_addr: std_logic_vector(7 downto 0):= "00000000";
+begin
+
+process(clk_wr)
+begin
+	if(clk_wr'event and clk_wr = '1') then	
+		full <= '0';
+		if(wr = '1') then
+			ram(conv_integer(wr_addr)) <= din;
+			wr_addr <= wr_addr + "00000001";
+			if(wr_addr = "11111111") then 
+				ram(conv_integer(wr_addr)) <= din;
+				full <= '1';
+				wr_addr <= "00000000";
+			end if;
+		end if;
+	end if;
+end process;
+
+process(clk_rd)
+begin
+	if(clk_rd'event and clk_rd = '1') then 	
+		empty <= '0';
+		if(rd = '1') then
+			dout <= ram(conv_integer(rd_addr));
+			rd_addr <= rd_addr + "00000001";
+			if(rd_addr = "11111111") then 
+				dout <= ram(conv_integer(rd_addr));
+				rd_addr <= "00000000";
+			if(rd_addr = wr_addr) then
+				empty <= '1';
+			end if;
+			end if;
+		end if;
+	end if;
+end process;
+
+end Behavioral;
---
+++ FIFO/FIFO256_t1.vhd
@@ -0,0 +1,93 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   18:44:32 05/07/2021
+-- Design Name:   
+-- Module Name:   D:/ISE/FIFO256/FIFO256_t1.vhd
+-- Project Name:  FIFO256
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: FIFO256
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY FIFO256_t1 IS
+END FIFO256_t1;
+ 
+ARCHITECTURE behavior OF FIFO256_t1 IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT FIFO256
+    PORT(
+         din : IN  std_logic_vector(7 downto 0);
+         clk_rd : IN  std_logic;
+         clk_wr : IN  std_logic;
+         wr : IN  std_logic;
+         rd : IN  std_logic;
+         dout : OUT  std_logic_vector(7 downto 0);
+         full : OUT  std_logic;
+         empty : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal din : std_logic_vector(7 downto 0) := (others => '0');
+   signal clk_rd : std_logic := '0';
+   signal clk_wr : std_logic := '0';
+   signal wr : std_logic := '0';
+   signal rd : std_logic := '0';
+
+ 	--Outputs
+   signal dout : std_logic_vector(7 downto 0);
+   signal full : std_logic;
+   signal empty : std_logic;
+
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: FIFO256 PORT MAP (
+          din => din,
+          clk_rd => clk_rd,
+          clk_wr => clk_wr,
+          wr => wr,
+          rd => rd,
+          dout => dout,
+          full => full,
+          empty => empty
+        );
+
+clk_rd <= not clk_rd after 30 ns;
+clk_wr <= not clk_wr after 30 ns;
+din <= "01011011" after 100 ns , "11111000" after 200 ns , "10100011" after 300 ns , "10100010" after 400 ns ,
+		"10000001" after 500 ns , "10101011" after 600 ns, "11110011" after 700 ns, "00011110" after 800 ns,
+		"11111100" after 900 ns, "01011100" after 1000 ns, "10111000" after 1100 ns, "00100001" after 1200 ns;
+wr <= '1' after 200 ns, '0' after 400 ns, '1' after 600 ns, '0' after 800 ns,
+			'1' after 1000 ns, '0' after 1200 ns;
+rd <= '0' after 200 ns, '1' after 400 ns, '0' after 600 ns, '1' after 800 ns,
+			'0' after 1000 ns, '1' after 1200 ns;
+
+END;
